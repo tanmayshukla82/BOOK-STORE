a71aabf9ae14828ec66f1729a30a7439a69762b7
tanmayshukla82 <67494383+tanmayshukla82@users.noreply.github.com>
2022-04-06 01:13:36 +0530

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,14 +1,29 @@
-const data = JSON.parse(localStorage.getItem("books"))
+const data = JSON.parse(localStorage.getItem("books")) || [];
 const cartLog = document.getElementById("cartLog");
+const checkout = document.getElementById("checkout");
 let tab = "";
+let sum = 0;
+if(data.length === 0)
+{
+  tab += `<div style="text-align:center">Your cart is empty</div>`
+}
+else{
 data.forEach((book) => {
+  let price = Math.floor(Math.random()*1000)+1;
+  sum += price;
   tab += `<tr>
             <td>
-                ${book.title};
+                ${book.title}
             </td>
             <td>
-                ${book.author};
+                ${book.author}
+            </td>
+            <td>
+                ${price}
             </td>
           </tr>`;
 });
+tab += `<tr><td></td><td><td>Total : ${sum}</td></tr>`
+tab += `<tr><td></td><td><td><Button id="checkout">Checkout</Button></td></tr>`;
+}
 cartLog.innerHTML = tab;
